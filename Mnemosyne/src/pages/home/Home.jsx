bb77957f58d3a5e5b1e2e68a423ac81408ea63b4
Sufiyan Shaikh <sufiyan.m.shaikh248@gmail.com>
2024-07-14 11:46:38 +0530

    added redux

--- Mnemosyne/src/pages/home/Home.jsx
+++ Mnemosyne/src/pages/home/Home.jsx
@@ -4,13 +4,6 @@ import { useDispatch, useSelector } from "react-redux";
 import { getAllBooks } from "./../../redux/actions/book.js";
 
 const Home = () => {
-  const { books, loading } = useSelector(state => state.allbooks);
-  const dispatch = useDispatch();
-
-  console.log("Books" + books);
-  useEffect(() => {
-    dispatch(getAllBooks())
-  }, [dispatch]);
   return <div>Home</div>;
 };
 
